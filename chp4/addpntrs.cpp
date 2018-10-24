03401e859df4daac5b5ecd3ea2ebb2f6a6ee552d
gaussic <dzkangshu@hotmail.com>
2018-10-24 23:30:56 +0800

    4.9 类型组合

---
+++ chp4/addpntrs.cpp
@@ -0,0 +1,36 @@
+// addpntrs.cpp -- pointer addition
+#include <iostream>
+int main()
+{
+    using namespace std;
+    double wages[3] = {10000.0, 20000.0, 30000.0};
+    short stacks[3] = {3, 2, 1};
+    
+    cout << stacks << " " << sizeof(stacks) << endl;
+    cout << stacks + 1 << " " << sizeof(stacks) << endl;
+    cout << &stacks << " " << sizeof(&stacks) << endl;
+    cout << &stacks + 1 << " " << sizeof(&stacks) << endl;
+
+    // Here are two ways to get the address
+    double * pw = wages;        // name of an array = address
+    short * ps = &stacks[0];    // or use address operator with array element
+    cout << "pw = " << pw << ", *pw = " << *pw << endl;
+    pw = pw + 1;
+    cout << "add 1 to the pw pointer:\n";
+    cout << "pw = " << pw << ", *pw = " << *pw << "\n\n";
+    cout << "ps = " << ps << ", *ps = " << *ps << endl;
+    ps = ps + 1;
+    cout << "add 1 to the ps pointer:\n";
+    cout << "ps = " << ps << ", *ps = " << *ps << "\n\n";
+
+    cout << "access two elements with array notation\n";
+    cout << "stacks[0] = " << stacks[0]
+         << ", stacks[1] = " << stacks[1] << endl;
+    cout << "access two elements with pointer notation\n";
+    cout << "*sttacks = " << *stacks
+         << ", *(stacks + 1) = " << *(stacks + 1) << endl;
+
+    cout << sizeof(wages) << " = size of wages array\n";
+    cout << sizeof(pw) << " = size of pw pointer\n";
+    return 0;
+}
